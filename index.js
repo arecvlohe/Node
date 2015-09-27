286d1d47950cad4061a5fd453081fa2f4a2a7e7a
Adam Recvlohe <adam.recvlohe@gmail.com>
2015-09-27 11:03:28 -0400

    refactor code to make it more composable and add ignore files

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var server = require('./server');
+var server  = require('./server');
 var weather = require('./weather');
+var data    = require('./data');
 
-server.start(weather.getWeather);
+server.start(weather.getWeather, data);
--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 var http = require('http');
-var data = require('./data');
-var temp = data.main.temp.toFixed(1);
-var tempDisplay = '{\r' + '\040\040\040' + 'currentTemperature: ' + temp +'\n}';
 
-function start(getWeather) {
+function start(getWeather, data) {
   function onRequest(request, response) {
+    var temp = data.main.temp.toFixed(1);
+    var tempDisplay = '{\r' + '\040\040\040' + 'currentTemperature: ' + temp +'\n}';
     response.writeHeader(200, {'Content-Type':'application/json'});
     response.end(tempDisplay, 'utf-8');
   }
