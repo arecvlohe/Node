4976c5de69e570b5b2a187ac698b2057b7200622
Adam Recvlohe <adam.recvlohe@gmail.com>
2015-09-27 22:21:01 -0400

    add tests

---
+++ test/test.js
@@ -0,0 +1,24 @@
+var chai       = require('chai');
+var expect     = require('chai').expect;
+var superagent = require('superagent');
+
+var weather    = require('../_weather');
+var start      = require('../_server');
+
+
+describe('start()', function() {
+  it('starts a server on localhost:5000', function() {
+    superagent
+      .get('htt://localhost:5000')
+      .end(function(res) {
+        expect(res.status).to.equal(200);
+        done()
+      })
+  })
+})
+
+describe('getWeather()', function() {
+  it('creates the data.json file', function() {
+    expect('data.json').to.exist;
+  })
+})
